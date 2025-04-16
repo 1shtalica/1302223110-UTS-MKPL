ae324a9e34484aad9d520653bcef78ba403e3f04
1shtalica <adibfaizulhaq05@gmail.com>
2025-04-16 20:47:56 +0700

    Memperbaiki bad smell data clumps, yang mana muncul di kelas employee

---
+++ TaxCalculator/src/lib/Person.java
@@ -0,0 +1,23 @@
+package lib;
+
+public class Person {
+    private String name;
+    private String ID;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String id) {
+        ID = id;
+    }
+
+}
