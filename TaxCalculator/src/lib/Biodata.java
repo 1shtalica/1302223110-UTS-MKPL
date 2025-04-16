65585691e047045dceef93f2e4d0b2104174da44
1shtalica <adibfaizulhaq05@gmail.com>
2025-04-16 21:35:13 +0700

    Membuat & merefactor code kelas employee agar lebih bersih

---
+++ TaxCalculator/src/lib/Biodata.java
@@ -0,0 +1,40 @@
+package lib;
+
+public class Biodata {
+    private String employeeId;
+    private String firstName;
+    private String lastName;
+    private String address;
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
